72cbca1a90a1e1fc3e97a5610c0ec01506e6e420
Juanma <carrascojm@gmail.com>
2022-09-20 20:23:01 -0300

    Versión inicial

---
+++ src/pokemon/pokemon.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { ParseMongoIdPipe } from '../common/pipes/parse-mongo-id.pipe';
+
+@Controller('pokemon')
+export class PokemonController {
+  constructor(private readonly pokemonService: PokemonService) {}
+
+  @Post()
+  create(@Body() createPokemonDto: CreatePokemonDto) {
+    return this.pokemonService.create(createPokemonDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.pokemonService.findAll();
+  }
+
+  @Get(':val')
+  findOne(@Param('val') val: string) {
+    return this.pokemonService.findOne( val );
+  }
+
+  @Patch(':val')
+  update(@Param('val') val: string, @Body() updatePokemonDto: UpdatePokemonDto) {
+    return this.pokemonService.update(val, updatePokemonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseMongoIdPipe) id: string) {
+    return this.pokemonService.remove(id);
+  }
+}
